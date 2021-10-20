3ccd35bd69fc717c69a6598fd1a1e20f4fc8a439
Steven J. Homem <sjhomem@outlook.com>
2021-10-20 16:39:18 -0500

    heroku push

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,29 +1,30 @@
 exports.up = async (knex) => {
-  await knex.schema.createTable("users", (users) => {
-    users.increments("user_id");
-    users.string("fullName", 200).notNullable();
-    users.string("email", 100).notNullable();
-    users.string("username", 200).notNullable().unique();
-    users.string("password", 128).notNullable();
-    users.string("role", 20).defaultTo("guest");
-  });
-  await knex.schema.createTable("potlucks", (potluck) => {
-    potluck.increments("potluck_id");
-    potluck
-      .integer("user_id")
-      .unsigned()
-      .notNullable()
-      .references("user_id")
-      .inTable("users");
-    potluck.string("potluckName", 120).notNullable();
-    potluck.string("streetAddress", 120).notNullable();
-    potluck.string("city", 120).notNullable();
-    potluck.string("state", 120).notNullable();
-    potluck.string("zipCode", 120).notNullable();
-    potluck.string("dayMonthYear", 120).notNullable();
-    potluck.string("timeOfPotluck", 120).notNullable();
-    potluck.string("specialInstructions", 500).notNullable();
-  });
+  await knex.schema
+    .createTable("users", (users) => {
+      users.increments("user_id");
+      users.string("fullName", 200).notNullable();
+      users.string("email", 100).notNullable();
+      users.string("username", 200).notNullable().unique();
+      users.string("password", 128).notNullable();
+      users.string("role", 20).defaultTo("guest");
+    })
+    .createTable("potlucks", (potluck) => {
+      potluck.increments("potluck_id");
+      potluck
+        .integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("user_id")
+        .inTable("users");
+      potluck.string("potluckName", 120).notNullable();
+      potluck.string("streetAddress", 120).notNullable();
+      potluck.string("city", 120).notNullable();
+      potluck.string("state", 120).notNullable();
+      potluck.string("zipCode", 120).notNullable();
+      potluck.string("dayMonthYear", 120).notNullable();
+      potluck.string("timeOfPotluck", 120).notNullable();
+      potluck.string("specialInstructions", 500).notNullable();
+    });
 };
 
 exports.down = async (knex) => {
